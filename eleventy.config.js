3be6d91b78d4c2cf28ff053ec237c6a292c99910
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2025-02-14 19:46:51 -0500

    Add fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
 
+    eleventyConfig.addPassthroughCopy("fonts");
+
     eleventyConfig.addCollection("post", function(collectionApi) {
         return collectionApi.getFilteredByTag("post").sort((a, b) => {
             return b.date - a.date; // Sort newest first
